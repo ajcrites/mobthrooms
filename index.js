ded4e7fe0ff5a50e7c8bc26cb7fe8ccf1a0055ec
Andrew Crites <acrites@mobiquityinc.com>
2015-10-25 16:01:15 -0400

    Improving comments

--- index.js
+++ index.js
@@ -11,23 +11,33 @@ let socketIo = require('socket.io');
 app.use(serveStatic(__dirname + "/public"));
 
 let particleAccessToken = process.env.PARTICLE_ACCESS_TOKEN;
+// Synchronize device list fetching
 let sparkListPromise;
 
+// If particle access token is provided we can use the devices
+// Othewrise, we create a test device list in memory
 if (particleAccessToken) {
     spark.login({accessToken: particleAccessToken});
     sparkListPromise = co(function* () {
+        // Retreive device list
         let devices = yield spark.listDevices();
+
+        // Get all attributes for devices
         let deviceAttributesList = yield devices.map(device =>
             co(function* () {
+                // `device.getAttributes` does not return a promise
                 let deviceAttributes = yield Promise.promisify(device.getAttributes, device)();
                 return {device: device, attributes: deviceAttributes};
             })
         );
+
+        // Filter these down to devices that have an `ocupado` attribute
         let occupiableDevices = yield deviceAttributesList.filter(
             deviceItem => deviceItem.attributes.variables
             && undefined !== deviceItem.attributes.variables.ocupado
         );
 
+        // Get the name and ocupado properties from the filtered device list
         return yield occupiableDevices.map(deviceItem =>
             co(function* () {
                 let [name, ocupado] = yield [
@@ -52,6 +62,7 @@ co(function* () {
     let server = http.createServer(app);
     let port = process.env.PORT || 3000;
 
+    // Create the socket connection and the server
     let [io, deviceList] = yield [
         socketIo(server),
         sparkListPromise,
@@ -66,10 +77,10 @@ co(function* () {
         socket.on("device-request", () => socket.emit("device-list", deviceList));
     });
 
-    // TODO unnamed device in event stream causes it to go crazy
-    // Need to test this with more specific device list or perhaps create
-    // individual device event listeners and emit to individual sockets
     if (particleAccessToken) {
+        // When occupancy has changed, we update the device list (for new
+        // user connections) and send the updated device to currently
+        // connected clients
         spark.getEventStream("occupancy-change", false, data => co(function* () {
             let device = yield spark.getDevice(data.coreid);
             let [name, isOccupied] = yield [
@@ -79,6 +90,7 @@ co(function* () {
             let deviceUpdate = {name: name.result, ocupado: isOccupied.result};
             let currentDeviceIndex = deviceList.findIndex(search => search.name === name);
 
+            // Device with this name was not in the list, so we add it
             if (-1 === currentDeviceIndex) {
                 deviceList.push(deviceUpdate);
             }
@@ -87,10 +99,10 @@ co(function* () {
             }
 
             io.emit("occupancy-change", deviceUpdate);
-        }));
+        }).catch(err => console.log(err.stack)));
     }
     else {
-        console.log("setting up two test devices");
+        console.log("setting up test devices");
         let ocupado5 = false;
         let ocupado18 = true;
         let ocupado30 = false;
@@ -103,6 +115,8 @@ co(function* () {
             ocupado18 = !ocupado18;
             io.emit("occupancy-change", {name: "test every 18 seconds", ocupado: ocupado18});
         }, 18000);
+
+        // Not on the initial list to test frontend handling of added devices
         setInterval(() => {
             ocupado30 = !ocupado30;
             io.emit("occupancy-change", {name: "test every 30 seconds and new", ocupado: ocupado30});
